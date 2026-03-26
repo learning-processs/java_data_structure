bb43cd8fb12740e97bc6eb11faabca793559fd49
Anu025 <anu001125@gmail.com>
2026-03-26 23:28:55 +0530

    _

---
+++ Java_DS/Maths/IsPrime.java
@@ -0,0 +1,18 @@
+package Maths;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(isPrime(n));
+    }
+
+    static boolean isPrime(int n){
+        // if(n <= 1 ) return false;
+        for( int i = 2 ; i< n ; i++){
+            if(n%i== 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
