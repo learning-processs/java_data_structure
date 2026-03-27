908a5adc41d1fd8eddeb6239f297f712b78d5824
Anu025 <anu001125@gmail.com>
2026-03-27 23:55:49 +0530

    NewtonSqrt

---
+++ Java_DS/Maths/NewtonSqrt.java
@@ -0,0 +1,22 @@
+package Maths;
+
+public class NewtonSqrt {
+    public static void main(String[] args) {
+        
+    }
+
+
+    static double sqrt(double n){
+        double x = n;
+        double root;
+
+        while (true) {
+             root = 0.5 * ( x + (n/x));
+
+            if(Math.abs(root - x) < 1){
+                break;
+            }
+            x = root;
+        }
+    }
+}
