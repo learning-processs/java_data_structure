94016fd25550b713ba526eda1ec2f6f981ef8191
Anu025 <anu001125@gmail.com>
2026-04-02 23:42:36 +0530

    selection Sort

--- Java_DS/sort/SelectionSort.java
+++ Java_DS/sort/SelectionSort.java
@@ -18,7 +18,7 @@ public class SelectionSort {
 
     static void selection(int arr[]){
         for(int i = 0 ; i < arr.length ; i++){
-            int last = arr.length - i -1;
+            int last = arr.length - i - 1;
             int maxIndex = getMaxIndex(arr, 0 , last);
             swap(arr, maxIndex, last);
         }
