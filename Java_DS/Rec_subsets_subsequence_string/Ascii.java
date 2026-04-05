d25aa73e67b7fe5da78f22dd227dd74b682d2564
Anu025 <anu001125@gmail.com>
2026-04-06 00:27:32 +0530

    Subset Problem

---
+++ Java_DS/Rec_subsets_subsequence_string/Ascii.java
@@ -0,0 +1,11 @@
+package Rec_subsets_subsequence_string;
+
+public class Ascii {
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch1 = 'b';
+
+        System.out.println(ch + 0);
+        System.out.println(ch1 + 0);
+    }
+}
--- Java_DS/Rec_subsets_subsequence_string/SubSequence.java
+++ Java_DS/Rec_subsets_subsequence_string/SubSequence.java
@@ -1,5 +1,7 @@
 package Rec_subsets_subsequence_string;
 
+import java.util.ArrayList;
+
 // [3, 5, 9] => [3], [5], [9], [3, 5],[3,9] [5, 9], [3,5,9],
 // Can't change the order in subset;
 // here it ignore and take few element => pattern (subset)
@@ -9,6 +11,10 @@ public class SubSequence {
     public static void main(String[] args) {
         String str = "abc";  // a, b, c, ab, ac, bc, abc
         subSequence(str, " ");
+        subSequenceAscii("abc", " ");
+
+
+        System.out.println(subSequence2(str, " "));
         
     }
 
@@ -21,6 +27,31 @@ public class SubSequence {
         char ch = unprocessed.charAt(0);
         subSequence(unprocessed.substring(1), processed + ch);
         subSequence(unprocessed.substring(1), processed);
+    }
 
+    static ArrayList<String> subSequence2(String unprocessed ,String processed){
+        if(unprocessed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        char ch = unprocessed.charAt(0);
+        ArrayList<String> left = subSequence2(unprocessed.substring(1), processed + ch);
+        ArrayList<String> right =subSequence2(unprocessed.substring(1), processed);
+        left.addAll(right);
+        return left;
+    }
+
+    static void subSequenceAscii(String unprocessed ,String processed){
+        if(unprocessed.isEmpty()){
+            System.out.println(processed);
+            return;
+        }
+
+        char ch = unprocessed.charAt(0);
+        subSequenceAscii(unprocessed.substring(1), processed + ch);
+        subSequenceAscii(unprocessed.substring(1), processed);
+        subSequenceAscii(unprocessed.substring(1), processed + (ch + 0));
     }
 }
