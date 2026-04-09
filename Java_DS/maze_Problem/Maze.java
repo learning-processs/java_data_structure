202b57acc8984099ee022e8944eee170d437aeba
Anu025 <anu001125@gmail.com>
2026-04-10 00:28:46 +0530

    Maze Prob

--- Java_DS/maze_Problem/Maze.java
+++ Java_DS/maze_Problem/Maze.java
@@ -5,6 +5,8 @@ package maze_Problem;
 public class Maze {
     public static void main(String[] args) {
         System.out.println(count(3, 3));
+
+        pathPrint("", 3, 3);
     }
 
     static int count(int r, int c) {
@@ -16,5 +18,22 @@ public class Maze {
         int right = count(r, c - 1);
 
         return left + right;
+    }
+
+
+     static void pathPrint(String processed ,int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(processed);
+            return;
+        }
+
+        if(r > 1 ){
+            pathPrint(processed + 'D',r - 1, c); // left rec call
+        }
+        if(c > 1 ){
+            pathPrint(processed + 'R',r, c - 1); // right rec call
+
+        }
+
     }
 }
