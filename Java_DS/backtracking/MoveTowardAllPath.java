c2173bfad1d891d28875146757e1f0c9d5694ab5
Anu025 <anu001125@gmail.com>
2026-04-12 00:11:37 +0530

    maze problem

--- Java_DS/backtracking/MoveTowardAllPath.java
+++ Java_DS/backtracking/MoveTowardAllPath.java
@@ -1,6 +1,8 @@
 package backtracking;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MoveTowardAllPath {
     public static void main(String[] args) {
@@ -10,6 +12,12 @@ public class MoveTowardAllPath {
             {true, true, true},
         };
         System.out.println(allPath("", board , 0, 0));
+
+
+
+        int [][] path = new int[board.length][board[0].length];
+        allPathPrint("", board , 0, 0 , path,1);
+
     }
 
     static ArrayList<String> allPath(String processed, boolean maze[][], int r, int c) {
@@ -44,5 +52,48 @@ public class MoveTowardAllPath {
         
         maze[r][c] = true;
         return list;
+    }
+
+
+
+
+     static void allPathPrint(String processed, boolean maze[][], int r, int c , int[][] path , int step) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = step;
+            for(int[]arr : path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(processed);
+            System.out.println();
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        if (r < maze.length - 1) {
+           allPathPrint(processed + 'D', maze, r + 1, c , path , step+1); // Down rec call
+        }
+
+        if (r > 0) {
+            allPathPrint(processed + 'U', maze, r - 1, c, path , step+1); // Up rec call
+        }
+
+        if (c < maze[0].length - 1) {
+            allPathPrint(processed + 'R', maze, r, c + 1, path , step+1); // right rec call
+        }
+
+        if (c > 0) {
+            allPathPrint(processed + 'L', maze, r, c - 1, path , step+1); // left rec call
+
+        }
+        
+        maze[r][c] = true;
+        path[r][c] = 0;
+
     }
 }
