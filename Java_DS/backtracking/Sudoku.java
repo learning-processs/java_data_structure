d55137f63a682860f9b66060c9b27f42a8d6d45c
Anu025 <anu001125@gmail.com>
2026-04-16 23:32:44 +0530

    Sudoku

---
+++ Java_DS/backtracking/Sudoku.java
@@ -0,0 +1,107 @@
+package backtracking;
+
+public class Sudoku {
+    public static void main(String args[]) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solve(board)) {
+            display(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+
+    static boolean solve(int[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+
+        boolean emptyLeft = true;
+
+        // Find empty cell
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+            if (!emptyLeft) break;
+        }
+
+        // No empty cell → solved
+        if (emptyLeft) {
+            return true;
+        }
+
+        // Try numbers 1 to 9
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+
+                if (solve(board)) {
+                    return true; // ✅ important
+                }
+
+                board[row][col] = 0; // backtrack
+            }
+        }
+
+        return false;
+    }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+
+        // Check row
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 box
+        int sqrt = (int) Math.sqrt(board.length);
+        int rowStart = row - row % sqrt;
+        int colStart = col - col % sqrt;
+
+        for (int r = rowStart; r < rowStart + sqrt; r++) {
+            for (int c = colStart; c < colStart + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    static void display(int[][] board) {
+        for (int[] row : board) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
