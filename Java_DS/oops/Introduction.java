0a622dbc8219433d7b9a4378678fbe81c742865c
Anu025 <anu001125@gmail.com>
2026-04-18 23:51:47 +0530

    Oops Intro

--- Java_DS/oops/Introduction.java
+++ Java_DS/oops/Introduction.java
@@ -3,19 +3,25 @@ package oops;
 public class Introduction {
     public static void main(String[] args) {
 
-        Student s = new Student(); // student()=>constructor
-
+        Student s = new Student("Anu", 28, 9.4f); // student()=>constructor
+        Student s2 = new Student("Lia",32,2.3f);
         // s.name= "Anu";
         // s.rollNo= 28;
         // s.marks= 8.5f;
 
-        s.changeName("Lia");
-        s.greeting();
+        // s.changeName("Lia");
+        // s.greeting();
 
 
-        // System.out.println(s.name);
-        // System.out.println(s.rollNo);
-        // System.out.println(s.marks);
+        System.out.println(s.name);
+        System.out.println(s.rollNo);
+        System.out.println(s.marks);
+        System.out.println();
+
+        System.out.println(s2.name);
+        System.out.println(s2.rollNo);
+        System.out.println(s2.marks);
+
 
     }
 }
@@ -39,6 +45,14 @@ class Student {
         this.name = "Anu";
         this.rollNo = 28;
         this.marks = 9.0f;
+    }
+
+    this keyword just replacing the referance varible
+
+    Student(String name, int rollNo , float marks) { // Constructor
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
     }
 
 }
