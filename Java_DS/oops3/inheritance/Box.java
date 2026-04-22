d63d197e20e85f346d3d25e8d8c2ac1a634574f7
Anu025 <anu001125@gmail.com>
2026-04-22 23:51:54 +0530

    Inheritance

---
+++ Java_DS/oops3/inheritance/Box.java
@@ -0,0 +1,31 @@
+package oops3.inheritance;
+
+public class Box {
+    double l;
+    double h;
+    double w;
+
+    Box() {
+        this.l = -1;
+        this.h = -1;
+        this.w = -1;
+    }
+
+    Box(double side) {
+        this.w = side;
+        this.l = side;
+        this.h = side;
+    }
+
+    Box(double l, double h, double w) {
+        this.w = w;
+        this.l = l;
+        this.h = h;
+    }
+
+    Box(Box old){
+        this.h = old.h;
+        this.l = old.l;
+        this.w = old.w;
+    }
+}
