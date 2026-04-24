44ef29b8858c7fa3311e4f54daa056c09a3d5df5
Anu025 <anu001125@gmail.com>
2026-04-25 00:14:05 +0530

    Polymorphism

--- Java_DS/oops3/polymorphism/Circle.java
+++ Java_DS/oops3/polymorphism/Circle.java
@@ -1,6 +1,11 @@
 package oops3.polymorphism;
 
 public class Circle extends Shapes{
+
+    // This will run when obj of circle is created
+    // hence it is overriding the parent method
+    
+    @Override // this is called annotation
     void area(){
         System.out.println("Area of circle is pie * r * r");
     }
