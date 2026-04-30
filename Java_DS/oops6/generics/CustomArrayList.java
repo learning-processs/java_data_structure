344b48156ba3a6a54d004cd2463573d7a5a8a4a5
Anu025 <anu001125@gmail.com>
2026-05-01 00:43:55 +0530

    CustomArrayList

--- Java_DS/oops6/generics/CustomArrayList.java
+++ Java_DS/oops6/generics/CustomArrayList.java
@@ -1,17 +1,90 @@
 package oops6.generics;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CustomArrayList {
+
+    private int[] data;
+    private static int DEFAULT_SIZE=10;
+    private int size = 0;
+
+    public CustomArrayList() {
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public void add(int num) {
+        if (isFull()) {
+            resize();
+        }
+
+        data[size++] = num;
+    }
+
+
+    private void resize() {
+       int[] temp = new int[data.length * 2];
+
+       // copy in new Array;
+       for(int i = 0 ; i < data.length ; i++){
+        temp[i] = data[i];
+       }
+
+       data = temp;
+    }
+
+    public boolean isFull() {
+        return size == data.length;
+    }
+
+
+    public int remove(){
+        int removed = data[--size];
+        return removed;
+    }
+
+    public  int get(int index){
+        return data[index];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void set(int index, int value){
+        data[index] = value;
+    }
+
+    @Override
+    public String toString(){
+        return "CutomArrayList{" + 
+        "data=" + Arrays.toString(data) + 
+        ", size=" + size + 
+        '}';
+        
+    }
+
     public static void main(String[] args) {
-        ArrayList list = new ArrayList<>();
+
+        CustomArrayList list = new CustomArrayList();
         list.add(7);
-        list.add(25);
         list.add(16);
-        list.remove(0);
-        System.out.println(list.get(0));
-        list.set(1, 45);
-        list.size();
-        list.isEmpty();
+        list.add(25);
+        System.out.println(list);
+
+    /*
+        ArrayList list = new ArrayList<>();
+
+         * list.add(7);
+         * list.add(25);
+         * list.add(16);
+         * list.remove(0);
+         * System.out.println(list.get(0));
+         * list.set(1, 45);
+         * list.size();
+         * list.isEmpty();
+         */
+
     }
 }
