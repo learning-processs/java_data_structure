d9ee0edf75ba688373037934269e0e3c039dfb6f
Anu025 <anu001125@gmail.com>
2026-05-03 00:41:34 +0530

    cloning

--- Java_DS/oops6/cloning/Human.java
+++ Java_DS/oops6/cloning/Human.java
@@ -3,10 +3,12 @@ package oops6.cloning;
 public class Human implements Cloneable{
     int age ;
     String name ;
+    int arr[];
 
-    public Human(int age,  String name){
+    public Human(int age,String name){
         this.age = age;
         this.name =name;
+        this.arr = new int[] { 1,3,4,2,1};
     }
 
     public Human(Human other){
@@ -14,8 +16,18 @@ public class Human implements Cloneable{
         this.name = other.name;
     }
 
-
+    @Override
     public Object clone() throws CloneNotSupportedException{
-        return super.clone();
+        // return super.clone();
+
+        // shallow copy
+        Human twin = (Human)super.clone();
+
+        // make deep copy
+        twin.arr = new int[twin.arr.length];
+        for(int i = 0 ; i < twin.arr.length ; i++){
+            twin.arr[i] = this.arr[i];
+        }
+        return twin;
     }
 }
--- Java_DS/oops6/cloning/Main.java
+++ Java_DS/oops6/cloning/Main.java
@@ -1,5 +1,7 @@
 package oops6.cloning;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException{
         Human h1 = new Human(21 , "Anu");
@@ -8,6 +10,7 @@ public class Main {
 
         Human twin =(Human)h1.clone();
         System.out.println(twin.age + " " + twin.name);
+        System.out.println(Arrays.toString(twin.arr));
 
     }
 
