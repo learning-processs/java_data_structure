399dfc0c7486ef610bf7dda619174429f7f9428a
Anu025 <anu001125@gmail.com>
2026-05-26 00:53:41 +0530

    Binary Tree

---
+++ Java_DS/binarySearchTree/BinaryTree.java
@@ -0,0 +1,24 @@
+package binarySearchTree;
+
+public class BinaryTree {
+
+
+    public class Node {
+        int node;
+        Node left;
+        Node right;
+
+        public Node(int node){
+            this.node = node;
+        }
+
+         public Node(int node, Node left , Node right){
+            this.node = node;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
